85e617fefbc0dd8b211db98922b3dcc01a6e8913
Gaston <109183526+filgueiragaston@users.noreply.github.com>
2022-09-08 12:33:03 -0300

    arrays1

---
+++ arrays.js
@@ -0,0 +1,11 @@
+// Array con valores number. 
+let array = [1, 2, 3];
+console.log(array);
+
+let arrayVacio = [];
+console.log("Array vacio: ");
+console.log(arrayVacio);
+
+arrayVacio = [25, 36, 99];
+console.log("Agrego elementos: ");
+console.log(arrayVacio);
